16835160184b0d6c09201fce299978e7d1283561
Ivan <37978791+iivankozlov@users.noreply.github.com>
2020-10-29 17:40:30 +0100

    Upload ekg-example

---
+++ misc/msp430f5529-blink-led/Debug/subdir_rules.mk
@@ -0,0 +1,13 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Each subdirectory must supply rules for building sources it contributes
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES) $(GEN_MISC_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: MSP430 Compiler'
+	"/Users/ivan/Workspace/Software/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/bin/cl430" -vmspx --data_model=restricted --use_hw_mpy=F5 --include_path="/Users/ivan/Workspace/Software/ccs/ccs_base/msp430/include" --include_path="/Users/ivan/Workspace/Projects/msp430f5529-blink-led" --include_path="/Users/ivan/Workspace/Software/ccs/tools/compiler/ti-cgt-msp430_20.2.2.LTS/include" --advice:power=all --define=__MSP430F5529__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --silicon_errata=CPU21 --silicon_errata=CPU22 --silicon_errata=CPU23 --silicon_errata=CPU40 --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
