376fd2b8b2e807e67d0a1f53788913bc9084f15e
Ivan <37978791+iivankozlov@users.noreply.github.com>
2020-11-05 12:31:48 +0100

    Added clock

--- misc/msp430f5529-blink-led/blink.c
+++ misc/msp430f5529-blink-led/blink.c
@@ -4,11 +4,13 @@
 /* Function declarations */
 void initWatchdog(void);
 void initGPIO(void);
+void initClocks(void);
 
 /* Function definitions */
 void main(void){
     initWatchdog();
     initGPIO();
+    initClocks();
 
     GPIO_setOutputHighOnPin(GPIO_PORT_P4, GPIO_PIN7);
     GPIO_setOutputLowOnPin(GPIO_PORT_P1, GPIO_PIN0);
@@ -34,4 +36,17 @@ void initWatchdog(void) {
 void initGPIO(void) {
     GPIO_setAsOutputPin(GPIO_PORT_P1, GPIO_PIN0);
     GPIO_setAsOutputPin(GPIO_PORT_P4, GPIO_PIN7);
+}
+
+/* 
+ * Setup Clocking 
+ * ACLK = aux clock, slow & low-power;
+ * REFO = 32768 Hz, trimmed to 3,5%;
+ * */
+void initClocks() {
+	UCS_initClockSignal (
+		UCS_ACLK,				// Configure ACLK
+		UCS_REFOCLK_SELECT, 	// Set to REFO source
+		UCS_CLOCK_DIVIDER_1		// Set clock divider to 1
+	);
 }
