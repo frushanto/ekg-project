09689b89735e384fe267197010a8e854484f7370
Ivan <37978791+iivankozlov@users.noreply.github.com>
2020-11-05 11:15:08 +0100

    Added driverlib.h

--- misc/msp430f5529-blink-led/blink.c
+++ misc/msp430f5529-blink-led/blink.c
@@ -1,12 +1,14 @@
 #include <msp430.h>				
+#include <driverlib.h>
 
+/* Function declarations */
+void initWatchdog(void);
+void initGPIO(void);
 
-/**
- * blink.c
- */
-void main(void)
-{
-	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
+/* Function definitions */
+void main(void){
+    initWatchdog();
+    GPIO_
 	P1DIR |= 0x01;					// configure P1.0 as output
 
 	volatile unsigned int i;		// volatile to prevent optimization
@@ -16,4 +18,12 @@ void main(void)
 		P1OUT ^= 0x01;				// toggle P1.0
 		for(i=50000; i>0; i--);     // delay
 	}
+}
+
+void initWatchdog(void) {
+	WDTCTL = WDTPW | WDTHOLD;		// stop watchdog timer
+}
+
+void initGPIO(void) {
+
 }
