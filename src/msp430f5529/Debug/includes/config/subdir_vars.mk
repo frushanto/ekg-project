19af37841000a41474d42b2db7e9bbe5abdec9a6
Erik Thuery <thueryer75092@th-nuernberg.de>
2020-12-05 13:53:02 +0100

    Erik first commit

--- src/msp430f5529/Debug/includes/config/subdir_vars.mk
+++ src/msp430f5529/Debug/includes/config/subdir_vars.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
+SHELL = cmd.exe
+
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
 ../includes/config/adc_cfg.c \
@@ -22,16 +24,16 @@ OBJS += \
 ./includes/config/uart_cfg.obj 
 
 OBJS__QUOTED += \
-"includes/config/adc_cfg.obj" \
-"includes/config/clk_cfg.obj" \
-"includes/config/gpio_cfg.obj" \
-"includes/config/uart_cfg.obj" 
+"includes\config\adc_cfg.obj" \
+"includes\config\clk_cfg.obj" \
+"includes\config\gpio_cfg.obj" \
+"includes\config\uart_cfg.obj" 
 
 C_DEPS__QUOTED += \
-"includes/config/adc_cfg.d" \
-"includes/config/clk_cfg.d" \
-"includes/config/gpio_cfg.d" \
-"includes/config/uart_cfg.d" 
+"includes\config\adc_cfg.d" \
+"includes\config\clk_cfg.d" \
+"includes\config\gpio_cfg.d" \
+"includes\config\uart_cfg.d" 
 
 C_SRCS__QUOTED += \
 "../includes/config/adc_cfg.c" \
