dad1cf6a62644a19f8dac0d2e88bd8f07ef87e15
Erik Thuery <thueryer75092@th-nuernberg.de>
2020-12-06 20:18:28 +0100

    fir Filter mit gleitpunktzahlen

---
+++ src/msp430f5529/includes/config/fir_filter.h
@@ -0,0 +1,23 @@
+/*
+ * fir_filter.h
+ *
+ *  Created on: 05.12.2020
+ *      Author: Erik
+ */
+
+#ifndef INCLUDES_CONFIG_FIR_FILTER_H_
+#define INCLUDES_CONFIG_FIR_FILTER_H_
+
+#include <msp430.h>
+#include <driverlib.h>
+#include <std_types.h>
+
+#define filter_coef 101
+#define nc (filter_coef * 2) - 1
+
+/*function declaration*/
+void fir_filter_init();
+double fir_filter(int new_sample);
+
+
+#endif /* INCLUDES_CONFIG_FIR_FILTER_H_ */
