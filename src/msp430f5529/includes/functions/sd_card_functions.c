d183ab32f9bd8519febfc2186305e0e5acf1c19e
Ivan <37978791+iivankozlov@users.noreply.github.com>
2021-01-28 15:44:26 +0100

    Working on creating and writing to a new txt file

--- src/msp430f5529/includes/functions/sd_card_functions.c
+++ src/msp430f5529/includes/functions/sd_card_functions.c
@@ -2,9 +2,11 @@
 
 unsigned char MST_Data, SLV_Data;
 BYTE buffer[32];
+unsigned char filename[] = "/testfile.txt";
+unsigned char txbufferInit[] = "SD Card init successful";
 int result = 1;
-//unsigned char buffer[20];
 unsigned int size;
+unsigned int bytesWritten;
 
 void Init_FAT(void){
     errCode = -1;
@@ -13,14 +15,26 @@ void Init_FAT(void){
         errCode = f_mount(0, &fatfs);                       //mount drive number 0
         errCode = f_opendir(&dir, "/");				    	//root directory
 
-        errCode = f_open(&file, "/testfile.txt", FA_CREATE_ALWAYS | FA_WRITE);
+        errCode = f_open(&file, filename, FA_CREATE_ALWAYS | FA_WRITE);
         if (errCode != FR_OK)
             result = 0;                                     //used as a debugging flag
     }
-    unsigned int bytesWritten;
-    f_write(&file, "SD Card init successful", 23, &bytesWritten);
-    res = f_read(&file, buffer, sizeof(buffer), &size);
+    f_write(&file, txbufferInit, sizeof(txbufferInit), &bytesWritten);
+    f_close(&file);
+    tempFunc_WriteToFile("Blah blah");
+}
+
+void tempFunc_WriteToFile(const void *txbuff) {
+    const BYTE *txbuffer = txbuff;
+    // Create new txt file and write data
+    f_mkdir("/test");
+    f_open(&file, "/test/newfile.txt", FA_CREATE_ALWAYS | FA_WRITE);
+    //strcpy(txbuffer, "Test data Test data Test data");
+    f_write(&file, txbuffer, sizeof(txbuffer), &bytesWritten);
+
+    //res = f_read(&file, buffer, sizeof(buffer), &size);
     f_close(&file);
     // TODO line below to be tested
     //f_mount(0,0);       // unmount sd card if needed
 }
+
