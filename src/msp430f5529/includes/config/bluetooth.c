b04192f79397bd510523eb9474c2d1a522c216b5
--replace-all <--replace-all>
2021-01-30 19:03:53 +0100

    Added Bluetooth.c File

---
+++ src/msp430f5529/includes/config/bluetooth.c
@@ -0,0 +1,9 @@
+/*
+ * bluetooth.c
+ *
+ *  Created on: 30.01.2021
+ *      Author: Adrian
+ */
+
+
+#include "bluetooth.h"
---
+++ src/msp430f5529/includes/config/bluetooth.h
@@ -0,0 +1,13 @@
+/*
+ * bluetooth.h
+ *
+ *  Created on: 30.01.2021
+ *      Author: adria
+ */
+
+#ifndef INCLUDES_CONFIG_BLUETOOTH_H_
+#define INCLUDES_CONFIG_BLUETOOTH_H_
+
+
+
+#endif /* INCLUDES_CONFIG_BLUETOOTH_H_ */
