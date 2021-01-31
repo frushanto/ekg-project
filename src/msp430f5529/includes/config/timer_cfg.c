fde41df92a90f597e851733be148863a6199e8a6
Ivan <37978791+iivankozlov@users.noreply.github.com>
2021-01-31 14:52:49 +0100

    TimerA2 with 1kHz added

--- src/msp430f5529/includes/config/timer_cfg.c
+++ src/msp430f5529/includes/config/timer_cfg.c
@@ -1,75 +1,5 @@
 #include <includes/config/timer_cfg.h>
 
-/*
- * Date: 2021-01-22
- * Author: Ivan Kozlov
- * Description: Timer Setup Parameters
- * 
- * ============================ TIMER A ============================
- * Timer A is triggered by SMCLK = 20447232Hz.
- *
- * Timer A1 has a clock divider = 32, which decreases timer triggered
- * frequency down to 20447232Hz / 32 = 638976Hz.
- * Timer A1 triggered frequency = 638976Hz.
- *
- * Timer A2 has a clock divider = 32, which decreases timer triggered
- * frequency down to 20447232Hz / 32 = 638976Hz.
- * Timer A2 triggered frequency = 638976Hz.
- * 
- * Timer A1 is used for 1kHz interrupt routine.
- * Timer A2 is used for 100Hz interrupt routine.
- * =================================================================
- * 
- * ============================ TIMER B ============================
- * Timer B is triggered by ACLK = 32768Hz.
- * Timer B0 has a clock divider = 1, which has NO influence on timer
- * frequency. Timer B0 frequency = 32768Hz.
- * 
- * Timer B0 is used for 1Hz interrupt routine.
- * =================================================================
- */
-
-/* 
- * ===================== CONSTANTS CALCULATION =====================
- * 
- * Timer B0 constant calculation
- * ACLK = 32768Hz 
- * DIVIDER = 1
- * Compare value = ACLK / Desired frequency / DIVIDER
- * => Compare value = 32768Hz / 1Hz / 1
- * => Compare value = 32768
- */
-uint16_t const timer_b0_compare_value_1hz_aclk = 32768;
-
-/* 
- * Timer A1 constant calculation
- * SMCLK = 20447232Hz
- * DIVIDER = 32
- * Compare value = SMCLK / Desired frequency / DIVIDER
- * => Compare value = 20447232Hz / 1000Hz / 32
- * => Compare value = 639
- */
-uint16_t const timer_a1_compare_value_1khz_smclk = 639;
-
-/* 
- * Timer A2 constant calculation
- * SMCLK = 20447232Hz
- * DIVIDER = 32
- * Compare value = SMCLK / Desired frequency / DIVIDER
- * => Compare value = 20447232Hz / 100Hz / 32
- * => Compare value = 6390
- */
-uint16_t const timer_a2_compare_value_100hz_smclk = 6390;
-
-/*
- * Timer test 
- */
-uint32_t timer_a2_100hz_test = 0;
-uint32_t timer_a1_1khz_test = 0;
-uint32_t timer_b0_1hz_test = 0;
-
- /* ============================================================= */
-
 void Init_Timers() {
     Init_Timer_A();
 }
@@ -79,17 +9,35 @@ void Init_Timer_A() {
     // Start timer
     Timer_A_clearTimerInterrupt(TIMER_A1_BASE);
 
-    /*** Init TIMER_A1 sourced by SMCLK ***/
-    Timer_A_initUpModeParam param = {0};
-    param.clockSource = TIMER_A_CLOCKSOURCE_ACLK;
-    param.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
-    param.timerPeriod = 32768;
-    param.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
-    param.captureCompareInterruptEnable_CCR0_CCIE =
+    /*** Init TIMER_A1 sourced by ACLK ***/
+    Timer_A_initUpModeParam confTimerA1 = {0};
+    confTimerA1.clockSource = TIMER_A_CLOCKSOURCE_ACLK;
+    confTimerA1.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_1;
+    confTimerA1.timerPeriod = 32768;
+    confTimerA1.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
+    confTimerA1.captureCompareInterruptEnable_CCR0_CCIE =
         TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
-    param.timerClear = TIMER_A_DO_CLEAR;
-    param.startTimer = true;
-    Timer_A_initUpMode(TIMER_A1_BASE, &param);
+    confTimerA1.timerClear = TIMER_A_DO_CLEAR;
+    confTimerA1.startTimer = true;
+    Timer_A_initUpMode(TIMER_A1_BASE, &confTimerA1);
+
+    // Start timer
+    Timer_A_clearTimerInterrupt(TIMER_A2_BASE);
+
+    /*** Init TIMER_A2 sourced by SMCLK ***/ 
+    Timer_A_initUpModeParam confTimerA2 = {0};
+    confTimerA2.clockSource = TIMER_A_CLOCKSOURCE_SMCLK; // 20447232Hz
+    confTimerA2.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_32; // 32 -> 638976Hz
+    confTimerA2.timerPeriod = 639; // 638976Hz / 1000Hz = 639
+    // Test with approx. 2 Hz
+    //confTimerA2.clockSourceDivider = TIMER_A_CLOCKSOURCE_DIVIDER_64; // 64 -> 319488Hz
+    //confTimerA2.timerPeriod = 63897; // approx. 2 Hz
+    confTimerA2.timerInterruptEnable_TAIE = TIMER_A_TAIE_INTERRUPT_DISABLE;
+    confTimerA2.captureCompareInterruptEnable_CCR0_CCIE =
+        TIMER_A_CAPTURECOMPARE_INTERRUPT_ENABLE;
+    confTimerA2.timerClear = TIMER_A_DO_CLEAR;
+    confTimerA2.startTimer = true;
+    Timer_A_initUpMode(TIMER_A2_BASE, &confTimerA2);
 }
 
 //******************************************************************************
@@ -109,6 +57,24 @@ void TIMER1_A0_ISR (void)
     //Toggle P1.0
     GPIO_toggleOutputOnPin(
 		GPIO_PORT_P1,
-		GPIO_PIN0
-		);
+		GPIO_PIN0);
+}
+
+//******************************************************************************
+//
+//This is the TIMER2_A0 interrupt vector service routine.
+//
+//******************************************************************************
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector=TIMER2_A0_VECTOR
+__interrupt
+#elif defined(__GNUC__)
+__attribute__((interrupt(TIMER2_A0_VECTOR)))
+#endif
+void TIMER2_A0_ISR (void)
+{
+    GPIO_toggleOutputOnPin(
+        GPIO_PORT_P4,
+        GPIO_PIN7);
+
 }
